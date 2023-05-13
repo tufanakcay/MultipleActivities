1752a02ff86a774808e53bbfe342d57c26c3805b
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-13 20:09:14 +0300

    First Commit

--- app/src/main/java/com/tufanakcay/multipleactivities/SecondActivity.java
+++ app/src/main/java/com/tufanakcay/multipleactivities/SecondActivity.java
@@ -2,13 +2,30 @@ package com.tufanakcay.multipleactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        textView = findViewById(R.id.textView2);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userInput");
+        textView.setText(userName);
+
+    }
+
+    public void changeScreen (View view) {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
     }
 }
